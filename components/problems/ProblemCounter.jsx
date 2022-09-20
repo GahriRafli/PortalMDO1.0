ca377fdda2b425ad8bc6b3a4a5106a98299e2267
haritsf <haritsf@hotmail.com>
2022-09-20 15:01:45 +0700

    Change ProblemCounter to Arrow Function

--- components/problems/ProblemCounter.jsx
+++ components/problems/ProblemCounter.jsx
@@ -1,32 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // ini contoh component yang menggunakan class
 // biar ada sampel lain daripada arrow function
-class ProblemCounter extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-      counting: 0,
-      end: props.end,
-    };
-  }
+// class ProblemCounter extends React.Component {
+//   constructor(props) {
+//     super();
+//     this.state = {
+//       counting: 0,
+//       end: props.end,
+//     };
+//   }
 
-  counter = (end) => {
+//   counter = (end) => {
+//     for (let counting = 0; counting <= end; counting++) {
+//       setTimeout(() => {
+//         this.setState({ counting });
+//       }, 500);
+//     }
+//   };
+
+//   componentDidMount() {
+//     const end = this.state.end;
+//     this.counter(end);
+//   }
+
+//   render() {
+//     return this.state.counting;
+//   }
+// }
+
+const ProblemCounter = ({ end }) => {
+  const initialCountingData = 0;
+  const [countingData, setCountingData] = useState(initialCountingData);
+
+  useEffect(() => {
     for (let counting = 0; counting <= end; counting++) {
       setTimeout(() => {
-        this.setState({ counting });
+        setCountingData(counting);
       }, 500);
     }
-  };
+  });
 
-  componentDidMount() {
-    const end = this.state.end;
-    this.counter(end);
-  }
-
-  render() {
-    return this.state.counting;
-  }
-}
+  return countingData;
+};
 
 export default ProblemCounter;
