8d0be2712470edf5605c3edd9831564908351b4a
Agus Dwi Ismawan <agus90146173@corp.bri.co.id>
2023-07-20 16:41:46 +0700

    fix typo text home, add incident

--- pages/index.jsx
+++ pages/index.jsx
@@ -251,7 +251,7 @@ export default function Home({
         <div>
           <div className="grid grid-cols-1 gap-4 pb-8 sm:grid-cols-1">
             <h2 className="text-lg font-medium leading-6 text-gray-900">
-              Genneral Report
+              General Report
             </h2>
             <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
               {/* Card */}
